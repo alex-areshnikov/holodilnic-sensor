33b300b4dcc752f02f78ee5e6e50aa63c4cd209b
Alex Areshnikov <alex.areshnikov@gmail.com>
2020-08-12 11:40:47 -0600

    Add timestamp to logs

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,18 @@
 
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <NTPClient.h>
 
 #include <Config.h>
 
 DHTesp dhtTop;
 DHTesp dhtBottom;
 
-WiFiClient esp_client;
-PubSubClient mqttClient(esp_client);
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, UTC_MDT_OFFSET_SECONDS);
 
 float humidityTop, temperatureTop, humidityBottom, temperatureBottom;
 String fanOverride = "";
@@ -58,8 +62,39 @@ void processMqtt() {
   mqttClient.loop();
 }
 
+String getFullFormattedTime() {
+   time_t rawtime = timeClient.getEpochTime();
+   struct tm * ti;
+   ti = localtime (&rawtime);
+
+   uint16_t year = ti->tm_year + 1900;
+   String yearStr = String(year);
+
+   uint8_t month = ti->tm_mon + 1;
+   String monthStr = month < 10 ? "0" + String(month) : String(month);
+
+   uint8_t day = ti->tm_mday;
+   String dayStr = day < 10 ? "0" + String(day) : String(day);
+
+   uint8_t hours = ti->tm_hour;
+   String hoursStr = hours < 10 ? "0" + String(hours) : String(hours);
+
+   uint8_t minutes = ti->tm_min;
+   String minuteStr = minutes < 10 ? "0" + String(minutes) : String(minutes);
+
+   uint8_t seconds = ti->tm_sec;
+   String secondStr = seconds < 10 ? "0" + String(seconds) : String(seconds);
+
+   return yearStr + "-" + monthStr + "-" + dayStr + " " +
+          hoursStr + ":" + minuteStr + ":" + secondStr;
+}
+
 void mqttLog(String msg) {
   char bufffer[64];
+
+  timeClient.update();
+
+  msg = ("[" + getFullFormattedTime() + "] " + msg);
   msg.toCharArray(bufffer, sizeof(bufffer));
 
   mqttClient.publish(MQTT_HOLODILNIC_LOGS_TOPIC, bufffer);
@@ -199,6 +234,8 @@ void setup() {
     
   initializeWifi();
   initializeMqtt(); 
+
+  timeClient.begin();
 
   initializeOTA();
 }
